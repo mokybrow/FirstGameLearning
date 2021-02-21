382088b5e060186dccc48b140443cd0a1f900818
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-21 21:31:27 +0200

    MovementScript

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private Rigidbody _objectBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float forward = Input.GetAxisRaw("Vertical");
+        float right = Input.GetAxisRaw("Horizontal");
+        _objectBody.AddForce(new Vector3(right, 0, forward) * _speed * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
