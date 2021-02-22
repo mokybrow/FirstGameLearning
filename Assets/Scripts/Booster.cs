78a1fddd5b14cd3838130200ceef6b5b6a79ab8c
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-22 20:46:36 +0200

    Booster

---
+++ Assets/Scripts/Booster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Booster : MonoBehaviour
+{
+    [SerializeField]
+    private float _boostForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody>().AddForce(transform.up * _boostForce);
+        }
+    }
+}
