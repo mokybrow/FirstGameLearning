ff42ce536d0a32171a5dc27d4cde8a340d6e0986
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-22 20:49:39 +0200

    Update Scripts

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -6,19 +6,29 @@ public class MovementScript : MonoBehaviour
     private float _speed;
 
     [SerializeField]
+    private float _jumpForce;
+
+    [SerializeField]
     private Rigidbody _objectBody;
+
+    private float _jumpRayLength;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _jumpRayLength = transform.localScale.y / 2 + 0.3f;
     }
 
     void FixedUpdate()
     {
         float forward = Input.GetAxisRaw("Vertical");
         float right = Input.GetAxisRaw("Horizontal");
-        _objectBody.AddForce(new Vector3(right, 0, forward) * _speed * Time.deltaTime);
+        _objectBody.AddForce(new Vector3(right, 0, forward) * _speed * Time.fixedDeltaTime);
+
+        if (Input.GetKey(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, _jumpRayLength))
+        {
+            _objectBody.AddForce(Vector3.up * _jumpForce);
+        }
     }
     // Update is called once per frame
     void Update()
