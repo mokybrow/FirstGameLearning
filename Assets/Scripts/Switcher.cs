1b3e2b5ee577648a4bc2d4bb9c1976e3a840ed75
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-22 20:46:46 +0200

    Switcher

---
+++ Assets/Scripts/Switcher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Switcher : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _objectToSwitch;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _objectToSwitch.SetActive(!_objectToSwitch.activeSelf);
+        }
+        
+    }
+}
