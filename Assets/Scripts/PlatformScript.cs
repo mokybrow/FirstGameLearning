e14c95e2ac34ce6120095a57e15a5e4e3c179822
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-22 20:47:07 +0200

    PlatformScript

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject, 1.0f);
+        }
+    }
+}
