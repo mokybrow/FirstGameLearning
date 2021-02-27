6aa811117f16e50e0bba4a440248da707adcf6bd
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-27 20:58:37 +0200

    Len

---
+++ Assets/Scripts/Game/BonusScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BonusScript : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private GameObject electroEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
+            animator.SetTrigger("PickUp");
+            Invoke("ActivateEffect", 1.5f);
+            Destroy(gameObject.transform.parent.gameObject, 1.5f);
+        }   
+    }
+
+    void ActivateEffect()
+    {
+        GameObject ourEffect = Instantiate(electroEffect, transform.position, Quaternion.identity);
+        Destroy(ourEffect, 2f);
+
+    }
+}
---
+++ Assets/Scripts/Game/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public int lvlIndex;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(lvlIndex);
+        }
+    }
+}
