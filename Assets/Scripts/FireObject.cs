f2e968aa51ea44ee775bc753664286f92413f222
Mikhail Panin <64284425+mokybrow@users.noreply.github.com>
2021-02-27 14:00:47 +0200

    Particle system

---
+++ Assets/Scripts/FireObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FireObject : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pointToAppear;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = pointToAppear.position;
+        }
+    }
+}
